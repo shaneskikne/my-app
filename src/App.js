cb17bf99c04c3f5598c044122c2696d3446e49d1
Shane Skikne <shaneskikne@Shanes-MacBook-Pro-2.local>
2021-05-21 17:02:31 -0500

    typo:

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Shane's React App.
+          Shane's React App!
         </p>
         <a
           className="App-link"
